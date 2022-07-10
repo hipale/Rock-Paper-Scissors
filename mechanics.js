8e853491f654657cc1b44c465ab4b462eecd1596
hipale <hhemmhilb@gmail.com>
2022-07-10 14:09:18 +0200

    Add point dots Change the background Delete unnecesary functions

--- mechanics.js
+++ mechanics.js
@@ -1,40 +1,56 @@
+let s = 0;
+let playerSelection = prompt("What would you like to pick?: ");
+
+
+dots = [document.getElementById("dot0"), document.getElementById("dot1"), 
+document.getElementById("dot2"), document.getElementById("dot3"), 
+document.getElementById("dot4"), document.getElementById("dot5"), 
+document.getElementById("dot6"),]
 compNum = ["Paper", "Rock", "Scissors"];
 let computerPlay = () => {
     i = Math.floor(Math.random()*3);
 }
-
+computerPlay();
+let computerSelection = compNum[i];
 let playerWon = () => {
     console.log(`you won ${playerSelection} beats ${computerSelection}`);
+    s++;
+    console.log(s);  
 }
 
 let playerLost = () => {
 console.log(`you lost ${computerSelection} beats ${playerSelection}`);
+     s--;
+     console.log(s);
+    
 }
 
-computerPlay();
-let computerSelection = compNum[i];
-
-let playerSelection = prompt("What would you like to pick?: ")
-
-if (playerSelection == computerSelection) {
+let scoreGet = () => {
+if (playerSelection[0].toUpperCase() == computerSelection[0]) {
   console.log("it's a draw")
-} else if (playerSelection[0] == "p" || playerSelection[0] == "P") {
+} else if (playerSelection[0].toUpperCase() == "P") {
     if (computerSelection == "Rock") {
         playerWon()
     } else {
         playerLost()
     }
-} else if (playerSelection[0] == "r" || playerSelection[0] == "R") {
+} else if (playerSelection[0].toUpperCase() == "R") {
     if (computerSelection == "Scissors") {
         playerWon()
     } else {
         playerLost()
     }
- } else if (playerSelection[0] == "s" || playerSelection[0] == "S") {
+ } else if (playerSelection[0].toUpperCase() == "S") {
         if (computerSelection == "Paper") {
             playerWon()
         } else {
             playerLost()
         }
     }
-
+}
+scoreGet();
+if (s == -3) {
+    console.log("you lost");
+} else if (s == 3) {
+    console.log("you won");
+}

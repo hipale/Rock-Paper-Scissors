e07d08686010666fbb3517734c904b6ce0d86e53
hipale <hhemmhilb@gmail.com>
2022-07-10 19:17:36 +0200

    Add setTimeout Change the functions type Add new functions

--- mechanics.js
+++ mechanics.js
@@ -1,56 +1,71 @@
 let s = 0;
 let playerSelection = prompt("What would you like to pick?: ");
 
-
 dots = [document.getElementById("dot0"), document.getElementById("dot1"), 
 document.getElementById("dot2"), document.getElementById("dot3"), 
 document.getElementById("dot4"), document.getElementById("dot5"), 
 document.getElementById("dot6"),]
-compNum = ["Paper", "Rock", "Scissors"];
-let computerPlay = () => {
+
+let computerSelection;
+                 
+function computerPlay() {   
+    compNum = ["Paper", "Rock", "Scissors"];      
     i = Math.floor(Math.random()*3);
+computerSelection = compNum[i];
 }
 computerPlay();
-let computerSelection = compNum[i];
-let playerWon = () => {
-    console.log(`you won ${playerSelection} beats ${computerSelection}`);
-    s++;
-    console.log(s);  
-}
 
-let playerLost = () => {
-console.log(`you lost ${computerSelection} beats ${playerSelection}`);
-     s--;
-     console.log(s);
-    
-}
 
-let scoreGet = () => {
-if (playerSelection[0].toUpperCase() == computerSelection[0]) {
-  console.log("it's a draw")
-} else if (playerSelection[0].toUpperCase() == "P") {
-    if (computerSelection == "Rock") {
-        playerWon()
-    } else {
-        playerLost()
-    }
-} else if (playerSelection[0].toUpperCase() == "R") {
-    if (computerSelection == "Scissors") {
-        playerWon()
-    } else {
-        playerLost()
-    }
- } else if (playerSelection[0].toUpperCase() == "S") {
-        if (computerSelection == "Paper") {
+
+function scoreGet () {
+    if (playerSelection[0].toUpperCase() == computerSelection[0]) {
+      console.log("it's a draw")
+      playGame()
+    } else if (playerSelection[0].toUpperCase() == "P") {
+        if (computerSelection == "Rock") {
             playerWon()
         } else {
             playerLost()
         }
+    } else if (playerSelection[0].toUpperCase() == "R") {
+        if (computerSelection == "Scissors") {
+            playerWon()
+        } else {
+            playerLost()
+        }
+     } else if (playerSelection[0].toUpperCase() == "S") {
+            if (computerSelection == "Paper") {
+                playerWon()
+            } else {
+                playerLost()
+            }
+        }
     }
-}
-scoreGet();
-if (s == -3) {
-    console.log("you lost");
-} else if (s == 3) {
-    console.log("you won");
-}
+    scoreGet()
+function playerWon() {
+    if (s < 2) {
+        console.log(`you won ${playerSelection} beats ${computerSelection}`);
+        s++;
+        console.log(s); 
+        playGame() } else {
+            console.log("you won");
+        }
+    }
+function playerLost()  {
+    if (s > -2) {
+    console.log(`you lost ${computerSelection} beats ${playerSelection}`);
+         s--;
+         console.log(s); 
+         playGame() } else {
+            console.log("you lost");
+         }
+
+    }
+    function playGame() {
+        setTimeout(() => {
+        playerSelection = prompt("What would you like to pick?: ");
+        computerPlay()
+        scoreGet()
+    }, 1000)
+    }         
+    

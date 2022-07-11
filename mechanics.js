a19d98d12ee4015d6d2a4f1f53b2c2e9023d51f4
hipale <hhemmhilb@gmail.com>
2022-07-11 22:47:57 +0200

    Put the playWin and playLost arrays out of the function

--- mechanics.js
+++ mechanics.js
@@ -2,28 +2,29 @@ let pp = 0;
 let cp = 3;
 let playerSelection = prompt("What would you like to pick?: ");
 
-dots = [document.getElementById("dot0"), document.getElementById("dot1"), 
-document.getElementById("dot2"), document.getElementById("dot3"), 
-document.getElementById("dot4"), document.getElementById("dot5"), 
+dots = [document.getElementById("dot0"), document.getElementById("dot1"),
+document.getElementById("dot2"), document.getElementById("dot3"),
+document.getElementById("dot4"), document.getElementById("dot5"),
 document.getElementById("dot6")];
 
+playWin = ["Scissors", "Paper", "Rock"];
+playLost = ["Rock", "Scissors", "Paper"];
+
 let computerSelection;
-                 
+
 function computerPlay() {
-    compNum = ["Paper", "Rock", "Scissors"];   
-    playWin = ["Scissors", "Paper", "Rock"];
-    playLost = ["Rock", "Scissors", "Paper"];
-    i = Math.floor(Math.random()*3);
-computerSelection = compNum[i];
+    compNum = ["Paper", "Rock", "Scissors"];
+    i = Math.floor(Math.random() * 3);
+    computerSelection = compNum[i];
 }
 computerPlay();
 
 
 
-function scoreGet () {
+function scoreGet() {
     if (playerSelection[0].toUpperCase() == computerSelection[0]) {
-      console.log("it's a draw")
-      playGame()
+        console.log("it's a draw")
+        playGame()
     } else if (playerSelection[0].toUpperCase() == "P") {
         if (computerSelection == "Rock") {
             playerWon()
@@ -36,55 +37,57 @@ function scoreGet () {
         } else {
             playerLost()
         }
-     } else if (playerSelection[0].toUpperCase() == "S") {
-            if (computerSelection == "Paper") {
-                playerWon()
-            } else {
-                playerLost()
-            }
+    } else if (playerSelection[0].toUpperCase() == "S") {
+        if (computerSelection == "Paper") {
+            playerWon()
+        } else {
+            playerLost()
         }
     }
-    scoreGet()
+}
+scoreGet()
 function playerWon() {
     if (pp < 2) {
         console.log(`you won ${playWin[i]} beats ${computerSelection}`);
-        console.log(pp+1); 
-        playGame() } else {
-            console.log("you won"); 
-        }
-        addPP()
-        
+        console.log(pp + 1);
+        playGame()
+    } else {
+        console.log("you won");
     }
-function playerLost()  {
+    addPP()
+
+}
+function playerLost() {
     if (cp < 5) {
-    console.log(`you lost ${computerSelection} beats ${playLost[i]}`);
-         console.log(cp-2);
-         playGame() } else {
-            console.log("you lost");  
-         }
-         addCP()
-         
+        console.log(`you lost ${computerSelection} beats ${playLost[i]}`);
+        console.log(cp - 2);
+        playGame()
+    } else {
+        console.log("you lost");
     }
-    function playGame() {
-        setTimeout(() => {
+    addCP()
+
+}
+function playGame() {
+    setTimeout(() => {
         playerSelection = prompt("What would you like to pick?: ");
         computerPlay()
         scoreGet()
     }, 2000)
-    }         
-    function addPP() {
-        dots[pp].style.opacity = "1";
-        changeGreen()
-        pp++;
-    }
-    function addCP() {
-        dots[cp].style.opacity = "1";
-        changeRed()
-        cp++;
-    }
-    function changeGreen() {
-        dots[pp].style.backgroundColor = "green";
-    }
-    function changeRed() {
-        dots[cp].style.backgroundColor = "red";
-    }
+}
+function addPP() {
+    dots[pp].style.opacity = "1";
+    changeGreen()
+    pp++;
+}
+function addCP() {
+    dots[cp].style.opacity = "1";
+    changeRed()
+    cp++;
+}
+function changeGreen() {
+    dots[pp].style.backgroundColor = "green";
+}
+function changeRed() {
+    dots[cp].style.backgroundColor = "red";
+}

b2b4495a03162726dccebc79e072cb7045a03f9c
hipale <hhemmhilb@gmail.com>
2022-07-14 20:24:56 +0200

    Add new branch Create RPS buttons Clean the java script code Add event listener

--- mechanics.js
+++ mechanics.js
@@ -1,21 +1,19 @@
 let pp = 0;
 let cp = 3;
-let playerSelection = prompt("What would you like to pick?: ");
-checkIfSelect()
-function checkIfSelect() {
-while (playerSelection == null) {
-    playerSelection = prompt("What would you like to pick?: ");
-}
-}
 
 dots = [document.getElementById("dot0"), document.getElementById("dot1"),
 document.getElementById("dot2"), document.getElementById("dot3"),
 document.getElementById("dot4"), document.getElementById("dot5"),
 document.getElementById("dot6")];
 
-const playWin = ["Scissors", "Paper", "Rock"];
-const playLost = ["Rock", "Scissors", "Paper"];
+let button = document.querySelector("#playButt");
+let playerSelection;
+button.addEventListener("click", playRound);
 
+function playRound(e) {
+    playerSelection = e.target.className;
+    scoreGet()
+}
 let computerSelection;
 
 function computerPlay() {
@@ -26,22 +24,22 @@ function computerPlay() {
 computerPlay();
 
 function scoreGet() {
-    if (playerSelection[0].toUpperCase() == computerSelection[0]) {
+    if (playerSelection == computerSelection) {
         console.log("it's a draw")
         playGame()
-    } else if (playerSelection[0].toUpperCase() == "P") {
+    } else if (playerSelection == "Paper") {
         if (computerSelection == "Rock") {
             playerWon()
         } else {
             playerLost()
         }
-    } else if (playerSelection[0].toUpperCase() == "R") {
+    } else if (playerSelection == "Rock") {
         if (computerSelection == "Scissors") {
             playerWon()
         } else {
             playerLost()
         }
-    } else if (playerSelection[0].toUpperCase() == "S") {
+    } else if (playerSelection == "Scissors") {
         if (computerSelection == "Paper") {
             playerWon()
         } else {
@@ -49,35 +47,35 @@ function scoreGet() {
         }
     }
 }
-scoreGet()
+
 function playerWon() {
     if (pp < 2) {
-        console.log(`you won ${playWin[i]} beats ${computerSelection}`);
+        console.log(`you won ${playerSelection} beats ${computerSelection}`);
         console.log(pp + 1);
         playGame()
     } else {
         console.log("you won");
+        removeE()
     }
     addPP()
 
 }
 function playerLost() {
     if (cp < 5) {
-        console.log(`you lost ${computerSelection} beats ${playLost[i]}`);
+        console.log(`you lost ${computerSelection} beats ${playerSelection}`);
         console.log(cp - 2);
         playGame()
     } else {
         console.log("you lost");
+        removeE()
     }
     addCP()
 
 }
 function playGame() {
     setTimeout(() => {
-        playerSelection = prompt("What would you like to pick?: ");
-        checkIfSelect()
         computerPlay()
-        scoreGet()
+        playerSelection;
     }, 2000)
 }
 function addPP() {
@@ -92,17 +90,10 @@ function addCP() {
 }
 function changeGreen() {
     dots[pp].style.backgroundColor = "green";
-}
+};
 function changeRed() {
     dots[cp].style.backgroundColor = "red";
+};
+function removeE() {
+    button.removeEventListener("click", playRound);
 }
-
-let inp = "hello"
-let num = inp.length-1;
-
-const reverseString = function() {
-    for(num; num >= 0; num--) {
-     return inp[num];
-};
-};
-reverseString()
